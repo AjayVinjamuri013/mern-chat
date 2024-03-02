074b8f7acdbc9f69b2ab2e7903c799e32f1f6756
Ajay <420randykumar@gmail.com>
2024-03-02 13:08:29 -0600

    displaying avatars for users

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Avatar from "./Avatar";
 
 export default function Chat(){
   const [ws, setWs] = useState(null);
@@ -35,8 +36,9 @@ export default function Chat(){
               Mern Chat
             </div>
             {Object.keys(onlinePeople).map(userId => (
-              <div key={userId} className="border-b border-gray-950 py-2">
-                {onlinePeople[userId]}
+              <div key={userId} className="border-b border-gray-950 py-2 flex items-center gap-2">
+                <Avatar username={onlinePeople[userId]} userId={userId}/>
+                <span>{onlinePeople[userId]}</span>
               </div>
             ))}
           </div>
